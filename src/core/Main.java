e062a82532421a7adaa3b8a4439f046d8ae697f3
Code Help Tutor <1823890830@qq.com>
2024-04-06 06:20:30 +0000

    WeChat cstutorcs CS61B Project 3 BYOW 2024

---
+++ src/core/Main.java
@@ -0,0 +1,13 @@
+WeChat: cstutorcs
+QQ: 749389476
+Email: tutorcs@163.com
+package core;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // build your own world!
+
+
+    }
+}
---
+++ src/core/World.java
@@ -0,0 +1,10 @@
+WeChat: cstutorcs
+QQ: 749389476
+Email: tutorcs@163.com
+package core;
+
+public class World {
+
+    // build your own world!
+
+}
